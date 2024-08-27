6fb478117e82c9492d1014c756533fc4c6ee2146
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-26 22:20:31 -0300

    try: deploy without calendar

--- src/pages/reservation/index.tsx
+++ src/pages/reservation/index.tsx
@@ -12,7 +12,7 @@ import DeleteReservationModal from "../../components/modals/modalsReservation/de
 import WaitListModal from "../../components/modals/modalsReservation/waitList";
 import AvaliationModal from "../../components/modals/modalsReservation/modalAvaliation";
 import AddGuest from "../../components/modals/modalsReservation/addGuest";
-import CalendarComponent from "../../components/calendar";
+
 import Head from "next/head";
 
 type ReservationsProps = {
@@ -171,13 +171,7 @@ export default function Reservation() {
             <Link href="/settings"><b>Configurações</b></Link> do sistema.</p>
           </div>
 
-          <section className={styles.calendarArea}> 
-            <CalendarComponent
-            myReservationsList={myReservationsList}
-            allReservationsList={allReservationsList}
-            setDateValue={setCalendarDate}
-            />
-          </section>
+
 
               
               {myReservationsList.length > 0?(
